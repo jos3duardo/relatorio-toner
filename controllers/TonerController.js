9bd268645279eb2eea53db784dd4b73a313015f8
eduardo <eduardo.lopes@before.com.br>
2020-09-18 14:12:14 -0400

    crud com knex finalizado

--- controllers/TonerController.js
+++ controllers/TonerController.js
@@ -6,6 +6,60 @@ module.exports = {
         const results = await knex('toners ')
 
         return res.status(200).json(results)
-    }
+    },
+    async create (req, res, next) {
+        try{
+            const { modelo, marca, tipo, status, recarga, quantidade } = req.body
+
+            let toner = await knex('toners').insert(
+                req.body
+            )
+
+            return res.status(201).json({data: 'Registro com id ' +toner + ' criado com sucesso'})
+
+        } catch (error){
+            next(error)
+        }
+    },
+    async show (req, res, next) {
+        try{
+
+            const { id } = req.params
+
+            let results = await knex('toners').where({id})
+
+            return res.status(201).json(results)
+
+        } catch (error){
+            next(error)
+        }
+    },
+    async update (req, res, next) {
+        try{
+            const { modelo, marca, tipo, status, recarga, quantidade } = req.body
+            const { id } = req.params
+
+            let toner = await knex('toners').update(
+                req.body
+            ).where({id})
+
+            return res.status(201).json({data: 'Registro editado com sucesso'})
+
+        } catch (error){
+            next(error)
+        }
+    },
+    async delete (req, res, next) {
+        try{
+            const { id } = req.params
+
+            let toner = await knex('toners').where({id}).del()
+
+            return res.status(201).json({data: 'Registro apagado com sucesso'})
+
+        } catch (error){
+            next(error)
+        }
+    },
 
 }
--- routes.js
+++ routes.js
@@ -4,5 +4,9 @@ const routes = express.Router()
 const TonerController = require('./controllers/TonerController')
 
 routes.get('/toner', TonerController.index)
+routes.get('/toner/:id', TonerController.show)
+routes.post('/toner', TonerController.create)
+routes.put('/toner/:id', TonerController.update)
+routes.delete('/toner/:id', TonerController.delete)
 
 module.exports = routes
