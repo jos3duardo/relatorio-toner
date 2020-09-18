d86e0d6087ab6c842a8c0a02eb4af8bbb52c94ee
eduardo <eduardo.lopes@before.com.br>
2020-09-17 23:13:24 -0400

    criando tavela de Atendimentos

---
+++ database/tabelas.js
@@ -0,0 +1,28 @@
+class Tabelas {
+    init(conexao){
+        this.conexao = conexao
+
+        this.criarAtendimentos()
+    }
+
+    criarAtendimentos(){
+        const sql = `
+                CREATE TABLE Atendimentos (
+                    id int NOT NULL AUTO_INCREMENT, 
+                    tipo varchar(255) NOT NULL,
+                    modelo varchar(255) NOT NULL,
+                    marca varchar(255) NOT NULL,
+                    created_at datetime,
+                    updated_at datetime,
+                    primary key(id)
+                )`
+        this.conexao.query(sql, (erro) => {
+            if (erro){
+                console.log(erro)
+            }
+            console.log('tabela criada')
+        })
+    }
+}
+
+module.exports = new Tabelas
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpress')
-
 const conexao = require('./database/conexao')
+const Tabelas = require('./database/tabelas')
 
 conexao.connect( (erro) => {
        if (erro) {
            console.log(erro)
        }else{
            console.log('conectado no banco com sucesso')
+
+           Tabelas.init(conexao)
 
            const app = customExpress()
 
