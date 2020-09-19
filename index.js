ce4201b550ead0d3d7e13563b279aef27cf3e86e
eduardo <eduardo.lopes@before.com.br>
2020-09-18 22:23:27 -0400

    criando frontend

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const app = express()
 app.use(express.json() )
 app.use(routes)
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //not found
 app.use((req,res,next) => {
